9df6ca07076af79ca970f41c6bbf9e03b0bed54d
vvakame <vvakame+dev@gmail.com>
2018-05-24 19:11:31 +0900

    docs(boom): write godoc comment to all .go files

--- boom/interfaces.go
+++ boom/interfaces.go
@@ -6,6 +6,7 @@ import (
 	"go.mercari.io/datastore"
 )
 
+// FromContext make new Boom object with specified context.
 func FromContext(ctx context.Context) (*Boom, error) {
 	client, err := datastore.FromContext(ctx)
 	if err != nil {
@@ -14,6 +15,7 @@ func FromContext(ctx context.Context) (*Boom, error) {
 	return &Boom{Context: ctx, Client: client}, nil
 }
 
+// FromClient make new Boom object from specified datastore.Client.
 func FromClient(ctx context.Context, client datastore.Client) *Boom {
 	return &Boom{Context: ctx, Client: client}
 }
