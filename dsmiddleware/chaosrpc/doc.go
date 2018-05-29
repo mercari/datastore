2c395ea3d381fdb3ab86be96a4d6c86f465c7756
vvakame <vvakame+dev@gmail.com>
2018-05-29 19:05:44 +0900

    docs(dsmiddleware/chaosrpc): add godoc to chaosrpc package

--- dsmiddleware/chaosrpc/doc.go
+++ dsmiddleware/chaosrpc/doc.go
@@ -1,3 +1,6 @@
+/*
+Package chaosrpc generates chaos very efficiently!
+This package will randomly error all RPCs with a probability of 20%.
+Please use only for testing.
+*/
 package chaosrpc // import "go.mercari.io/datastore/dsmiddleware/chaosrpc"
-
-// This package MUST only be used for testing.
